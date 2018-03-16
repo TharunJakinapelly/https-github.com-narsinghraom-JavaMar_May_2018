b9362189e442ee01656fa3abdac9c34087c7d3b0
srilakshmi03 <srilakshmi.vtalent@gmail.com>
2018-03-16 10:13:05 +0530

    sample1

---
+++ CoreJava_MarToMay18/src/D.java
@@ -0,0 +1,12 @@
+
+public class D {
+int i,j;
+int k=i+j;
+	public static void main(String[] args) {
+		D d=new D();
+		// TODO Auto-generated method stub
+		System.out.println(d.k);
+
+	}
+
+}
--- CoreJava_MarToMay18/src/com/vtalent/branch/three/srilakshmi/C.java
+++ CoreJava_MarToMay18/src/com/vtalent/branch/three/srilakshmi/C.java
@@ -1,12 +1,9 @@
 package com.vtalent.branch.three.srilakshmi;
 
 public class C {
-		int i=5;
-		int j=6;
 		public static void main(String[] args) {
-			C c=new C();//object 
-			int k=c.i+c.j;
-			System.out.println(k);
+			
+			
 		}
 		}
 
