e038bdc3d3ee2830401b360e49864ac0ac8f49bb
srilakshmi03 <srilakshmi.vtalent@gmail.com>
2018-03-16 10:21:09 +0530

    edited d.java

---
+++ CoreJava_MarToMay18/src/com/vtalent/branch/three/srilakshmi/D.java
@@ -0,0 +1,13 @@
+package com.vtalent.branch.three.srilakshmi;
+
+public class D {
+int i,j;
+int k=i+j;
+	public static void main(String[] args) {
+		D d=new D();
+		// TODO Auto-generated method stub
+		System.out.println(d.k);
+//
+	}
+
+}
