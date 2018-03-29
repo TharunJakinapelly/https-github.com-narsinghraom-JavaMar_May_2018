143c2f79007b384cb746d7b0e83a697fa348a6de
srilakshmi03 <srilakshmi.vtalent@gmail.com>
2018-03-29 10:23:59 +0530

    commandLine argument

---
+++ CoreJava_MarToMay18/src/com/vtalent/branch/three/srilakshmi/commandLine.java
@@ -0,0 +1,10 @@
+package com.vtalent.branch.three.srilakshmi;
+
+public class commandLine {
+public static void main(String[] args) {
+	for(int i=0;i<=args.length-1;i++)
+	{
+		System.out.println(args[i]);
+	}
+}
+}
